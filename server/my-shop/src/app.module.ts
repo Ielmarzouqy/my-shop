60e82e7207bc80abc8659d76498948f22bafa3dd
MustaphaBousilDev <bousilmustapha@gmail.com>
2024-07-26 09:11:02 +0100

    add connection database in shared folder for Dynamic Import

--- server/my-shop/src/app.module.ts
+++ server/my-shop/src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModulemySQL } from './shared/database.mysql.module';
 
 @Module({
-  imports: [],
+  imports: [
+    DatabaseModulemySQL,
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
-  exports:[AppController]
+  exports: [],
 })
 export class AppModule {}
---
+++ server/my-shop/src/auth/user.entity.ts
@@ -0,0 +1,20 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  _id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  // i dont want to add Field into password , because password is sensitive data
+  password: string;
+}
